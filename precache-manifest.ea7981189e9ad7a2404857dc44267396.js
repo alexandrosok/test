3bd7f92b5304a69a8c73ec4738924ee1aebef22e
alexandrosok <alexandrosok@hotmail.com>
2020-10-15 15:51:40 +0100

    init

---
+++ precache-manifest.ea7981189e9ad7a2404857dc44267396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e271765a7e56fb159eab09f7242b7a2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23267e3b3f1d9810549a",
+    "url": "/static/css/main.1d0fada7.chunk.css"
+  },
+  {
+    "revision": "27290d47054e57aca7b5",
+    "url": "/static/js/2.d4eed1f0.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.d4eed1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23267e3b3f1d9810549a",
+    "url": "/static/js/main.a59c2d3b.chunk.js"
+  },
+  {
+    "revision": "bf97738b1b4580a3ea48",
+    "url": "/static/js/runtime-main.76ec213c.js"
+  },
+  {
+    "revision": "31f6d8e26091c2892aa03de117279ee7",
+    "url": "/static/media/bg.31f6d8e2.jpg"
+  }
+]);
